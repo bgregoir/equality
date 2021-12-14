7f0715133bf7619d68f0d115e0139b010beb5182
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-12-14 13:14:41 +0100

    wip

--- src/eqbcorrect.v
+++ src/eqbcorrect.v
@@ -20,7 +20,28 @@ Elpi Db eqcorrect.db lp:{{
     o:inductive,
     o:constant, % correct
     o:constant. % reflexive
+  /* JC HERE 
+
+  pred eqb-correct-aux-for o:term, o:term.
+  eqb-correct-aux-for
+    {{ @eq_correct_on lp:T lp:F }}
+    {{ (fun (a : lp:T) (H : @eq_correct_on lp:T lp:F) => H) }}.
+  %
+  eqb-correct-aux-for
+    {{ option_is_option lp:T (@eqb_correct_on _ _) }}
+    {{ (fun (a : lp:T) (H : option_eqb_correct_aux lp:T lp:Cmp }}.
+  eqb-correct-aux-for
+    {{ option_is_option lp:T lp:P }}
+    {{ fun x H => option_is_option_functor lp:T _ (@eqb_correct_on _ _) (lp:Rec x H) }} :-
+    eqb-correct-aux-for P Rec.
+
   
+  eqb-correct-aux-for {{ list_is_list lp:A lp:P }} {{ list_eqb_correct_aux ... }} :-
+    eqb-correct-aux-for P X.
+
+    option_is_option (list A) (list_is_list A (eqb_correct_on A FA))
+*/
+
 }}.
       
 Elpi Command eqbcorrect.
@@ -29,10 +50,12 @@ Elpi Accumulate Db eqb.db.
 Elpi Accumulate Db fields.db.
 Elpi Accumulate Db eqcorrect.db.
 Elpi Accumulate Db derive.induction.db.
-Elpi Accumulate Db derive.param1.db.
 Elpi Accumulate Db derive.param1.inhab.db.
 Elpi Accumulate File "src/elpi-ltac.elpi".
 Elpi Accumulate File "src/eqbcorrect.elpi".
+Elpi Accumulate File "src/paramX-lib.elpi".
+Elpi Accumulate File "src/param1.elpi".
+Elpi Accumulate Db derive.param1.db.
 Elpi Accumulate lp:{{
   main [str S] :-
     std.assert! (coq.locate S (indt I)) "Not an inductive type",
@@ -40,3 +63,5 @@ Elpi Accumulate lp:{{
     eqbcorrect.main I Prefix _.
 }}.
 Elpi Typecheck.
+
+Elpi Print eqbcorrect.
--- src/option_defs.v
+++ src/option_defs.v
@@ -1,14 +1,25 @@
 From elpi.apps Require Import derive.
 Require Import core_defs  tag fields eqb eqbcorrect eqbP.
 
-#[only(induction,param1_full,param1_trivial)] derive option.
+#[verbose,only(induction,param1_functor,param1_trivial)] derive option.
 Elpi tag     option.
 Elpi fields  option.
 Elpi eqb     option.
 Elpi eqbcorrect option.
 Elpi eqbP option.
+#[verbose,only(induction,param1_functor,param1_trivial)] derive list.
+Elpi tag     list.
+Elpi fields  list.
+Elpi eqb     list.
+Elpi eqbcorrect list.
+Elpi eqbP list.
 
+About option_is_option_functor.
+About list_is_list_functor.
 
+(* forall (A : Type) (PA1 PA2 : A -> Type),
+   (forall x : A, PA1 x -> PA2 x) ->
+    forall x : option A, option_is_option A PA1 x -> option_is_option A PA2 x *)
 
 Lemma option_eqb_correct_Some (A:Type) (eqA: A -> A -> bool) (a:A) : 
   eqb_correct_on eqA a -> eqb_correct_on (option_eqb A eqA) (Some a).
@@ -29,15 +40,12 @@ Proof.
   eqb_correct_on__solver.
 Qed.
   
-Lemma option_eqb_correct_aux (A : Type) (eqA : A -> A -> bool) :
-  let PA := @eqb_correct_on A eqA in
-  let P := @eqb_correct_on (option A) (option_eqb A eqA) in
-  forall (o : option A) (H:option_is_option A PA o), P o.
+Lemma option_eqb_correct_aux (A : Type) (eqA : A -> A -> bool) (o : option A) :
+     @option_is_option A          (@eqb_correct_on A eqA) o  ->
+     @eqb_correct_on   (option A) (option_eqb A eqA)      o.
 Proof.
-  move=> PA P.
-  apply: (@option_induction A PA P); rewrite /PA /P.
-  + apply option_eqb_correct_Some.
-  apply option_eqb_correct_None.
+  refine (@option_induction A _ _ (option_eqb_correct_Some A eqA)
+                                  (option_eqb_correct_None A eqA) o).
 Qed.
 
 Lemma option_eqb_correct' (A : Type) (eqA : A -> A -> bool) :
@@ -49,17 +57,22 @@ Proof.
 Qed.
 
 
+
+
+Lemma list_eqb_correct_aux (A : Type) (eqA : A -> A -> bool) (o : list A) :
+     @list_is_list     A          (@eqb_correct_on A eqA) o  ->
+     @eqb_correct_on   (list A)   (list_eqb A eqA)        o.
+Admitted.
+
 Inductive t := 
   | T0 
-  | T1 of option t.
+  | T1 of option (list (option t)).
 
 #[only(induction,param1_functor,param1_trivial)] derive t.
 Elpi tag     t.
 Elpi fields  t.
 Elpi eqb     t.
-Elpi eqbcorrect t.
-
-Check t_induction.
+(* Elpi eqbcorrect t. *)
 
 Lemma t_eqb_correct_T0 : eqb_correct_on t_eqb T0.
 Proof.
@@ -69,16 +82,34 @@ Proof.
   eqb_correct_on__solver.
 Qed.
 
-Lemma t_eqb_correct_T1 (o : option t) :
-  option_is_option t (eqb_correct_on t_eqb) o -> eqb_correct_on t_eqb (T1 o).
+Lemma t_eqb_correct_T1 (o : option (list (option t))) :
+  option_is_option (list (option t))
+    (list_is_list (option t)
+      (option_is_option t (eqb_correct_on t_eqb))) o ->
+   eqb_correct_on t_eqb (T1 o).
 Proof.
   move=> H.
-  (* This is the key point. If we don't do it here the tactic eqb_correct_on__solver does not work *)
-  have H' := option_eqb_correct_aux _ _ _ H.
+
+  refine 
+  (@eqb_body_correct t t_tag t_fields_t t_fields t_construct
+    t_constructP (t_eqb_fields t_eqb) (T1 o) (fun x => _)).
+  rewrite /= => W.
+  congr (Some (T1 _)).
+  move: x W.
+  change (eqb_correct_on (option_eqb _ (list_eqb (option t) (option_eqb t t_eqb))) o).
+
+  apply option_eqb_correct_aux.
+  move: o H; apply option_is_option_functor => o H.
+  apply list_eqb_correct_aux.
+  move: o H; apply list_is_list_functor => o H.
+  apply option_eqb_correct_aux.
+  by [].
+(*
   refine 
     (@eqb_body_correct t t_tag t_fields_t t_fields t_construct
       t_constructP (t_eqb_fields t_eqb) (T1 o) (fun x => _)).
   eqb_correct_on__solver.
+  *)
 Qed.
 
 Lemma t_eqb_correct_aux :
